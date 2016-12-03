7601b3fd9b735f86dbe497d219661c19bb5f1ce9
Moe Chughtai <chughtai.mh@gmail.com>
2016-12-03 18:54:11 -0500

    Updated RockPaperScissors.post method to include handler

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class RockPaperScissors(webapp2.RequestHandler):
         """Receives a POST request"""
 
         move = self.request.get('text')
-        play_rock_paper_scissors(move, self)
+        play_rock_paper_scissors(move, handler=self)
 
 
 def play_rock_paper_scissors(move, handler):
@@ -45,66 +45,10 @@ def play_rock_paper_scissors(move, handler):
                     },
         }
 
-    handler.response.write(game.get(move).get(comp))
-
-
-
-
-    # User picks rock
-    if move == "rock":
-        if comp == 'rock':
-            self.response.write('I chose Rock too. We tie')
-        elif comp == 1:
-            self.response.write('I chose Paper. You Lose')
-        else:
-            self.response.write('I chose Scissors. You Win')
-
-    # User picks paper
-    elif move == "paper":
-        if num == 1:
-            self.response.write('I chose Paper too. We Tie')
-        elif num == 2:
-            self.response.write('I chose Scissors. You Lose')
-        else:
-            self.response.write('I chose Rock. You Win')
-
-    # User picks scissors
-    elif move == "scissors":
-        if num == 2:
-            self.response.write('I chose Scissors too. We Tie')
-        elif num == 0:
-            self.response.write('I chose Rock. You Lose')
-        else:
-            self.response.write('I chose Paper. You Win')
-
-    # User put in an incorrect move
+    if move not in game.keys():
+        handler.response.write('{} is not a valid move'.format(move))
     else:
-        self.response.write("{}is not a valid move.".format(move))
-
-
-###############################################################
-# An alterative way to handle logic that is more maintainable #
-    # game = {
-    #         "rock": {
-    #                 1: "I chose Rock too. We Tie",
-    #                 2: "I chose Paper. You Lose",
-    #                 3: "I chose Scissors. You Win",
-    #                 },
-    #         "paper": {
-    #                 1: "I chose Paper too. We Tie",
-    #                 2: "I chose Scissors. You Lose",
-    #                 3: "I chose Rock. You Win",
-    #                 },
-    #         "scissors": {
-    #                 1: "I chose Scissors too. We Tie",
-    #                 2: "I chose Rock. You Lose",
-    #                 3: "I chose Paper. You Win",
-    #                 },
-    #         }
-    # if move not in game.keys():
-    #     self.response.write("{}is not a valid move.".format(move))
-
-    # self.response.write(game.get(move).get(num))
+        handler.response.write(game.get(move).get(comp))
 
 
 app = webapp2.WSGIApplication([
